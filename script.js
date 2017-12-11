be7655db0f6398e7be3053a2288cc9edcce5ec45
Kevon Eaglin <kceaglin@gmail.com>
2017-12-11 14:46:14 -0500

    Only top disk will move

--- script.js
+++ script.js
@@ -10,15 +10,16 @@ for (var i = 0; i < disks.length; i++) {
 function moveDisk(e) {
   // we want to get the parent of the disk being moved to present relevant options on where it can be moved next
   // we want to allow the user to move this disk to another post but only if
-    // 1) it's the top disk on its post
-    // 2) the post the user wants to move it to isn't the one it's on
-    // 3) the disk isn't bigger than the one on the target post
-
-  // for now, let's "move" a disk when clicked
-  var parent = e.target.parentNode
-  document.body.childNodes[5].childNodes[3].insertBefore(e.target, document.body.childNodes[5].childNodes[3].firstElementChild);
-  console.log('disk moved')
-  console.log(e)
+    // 1) it's the top disk on its own post
+    // 2) the disk isn't bigger than the one on the target post
+    // 3) the post the user wants to move it to isn't the one it's currently on
+  if (e.target.parentNode.firstElementChild.dataset.id === e.srcElement.dataset.id) {
+      // for now, let's "move" a disk when clicked
+    var parent = e.target.parentNode
+    document.body.childNodes[5].childNodes[3].insertBefore(e.target, document.body.childNodes[5].childNodes[3].firstElementChild);
+    console.log('disk moved')
+    console.log(e)
+  }
 }
 
 // e.toElement.dataset.id contains the data-id we assigned so that's one way to keep track of what's clicked
